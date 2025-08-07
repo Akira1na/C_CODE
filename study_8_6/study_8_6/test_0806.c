d0f803449fd60ed90397e922314903b793a17b75
Akira1na <Suna3530@outlook.com>
2025-08-07 10:54:10 +0800

    初识---作业1、操作符的使用

---
+++ study_8_6/study_8_6/test_0806.c
@@ -0,0 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//Work_1
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     Man\n");
+//
+//	return 0;
+//}
+
+////Work_2
+//int main()
+//{
+//	printf("I lost my cellphone!\n");
+//	return 0;
+//}
+
+//Work_3
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//	int sum = (-8 + 22) * a - 10 + c / 2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//Work_4
+//int MAX(int x, int y)
+//{
+//	if (x > y)
+//	{
+//		return x;
+//	}
+//	else
+//	{
+//		return y;
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int r = MAX(a, b);
+//	printf("%d\n", r);
+//
+//	return 0;
+//}
+
+//Work_5
+//int Out(int a,int b)
+//{
+//	if (a < 0)
+//	{
+//		return b = 1;
+//	}
+//	else if (a == 0)
+//	{
+//		return b = 0;
+//	}
+//	else
+//	{
+//		return b = -1;
+//	}
+//}
+//int main()
+//{
+//	//输入
+//	int x = 0;
+//	scanf("%d",&x);
+//	//输出
+//	int y = 0;
+//	printf("%d\n", Out(x, y));
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//操作符使用
+//int main()
+//{
+//	//--   ++  前置、后置使用
+//	int a = 1;
+//	int b = --a;
+//	printf("%d\n%d\n",a,b);
+//
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	int a = (int)3.14;  //(类型)强制类型转换   单目操作符
+//	printf("%d\n",a);
+//	return 0;
+//}
+//
